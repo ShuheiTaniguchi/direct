d12598d67bc3840f5ae3c5165a08c9baaff6e37c
shuhe <shuhe@192.168.1.3>
2025-07-02 18:40:32 +0900

    aaa

--- jikosyoukai/src/Ren2_33/Main.java
+++ jikosyoukai/src/Ren2_33/Main.java
@@ -4,10 +4,10 @@ public class Main {
  
 	public static void main(String[] args) {
 		  
-		Person  person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
+		Person  person1 = new Person("鈴木","太郎", 20, 1.7, 60); 
 		person1.print();
 		
-		Person  person2 = new Person("山田", "花子", 22, 1.5, 40);
+		Person  person2 = new Person("山田","花子", 22, 1.5, 40);
 		person2.print(); 
 		
 		Person.printCount();
